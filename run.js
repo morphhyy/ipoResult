50753c346e69be0a75b7ceba3a06cac63ae60dc5
morphhyy <imbibek05@gmail.com>
2021-09-23 17:39:53 +0545

    tryCatch

--- run.js
+++ run.js
@@ -6,41 +6,50 @@ const prompt = promptSync();
 const url = 'https://iporesult.cdsc.com.np/'
 
 const getData = async () => {
-    const res = await fetch("https://iporesult.cdsc.com.np/result/companyShares/fileUploaded");
-    const data = await res.json()
-    return data
+    try {
+        const res = await fetch(url + "result/companyShares/fileUploaded");
+        const data = await res.json()
+        return data
+    } catch(err) {
+        console.error("Error: ", err.message)
+        process.exit(1)
+    }
 }
 
-getData().then(data => {
-    
-    const details = []
-    data.body.map(company => {
-        details.push({id:company.id, name:company.name })
-    })
-    
-    details.map(a => console.log(a.id, '=>', a.name))
-    console.log('')
-    const userID = prompt("Choose: ")
-
-    const result = async (v) => {
-        const res = await fetch("https://iporesult.cdsc.com.np/result/result/check", {
-            "headers": {
-                "Content-Type": "application/json"
-            },
-            "body": `{"companyShareId":"${userID}","boid":"${v}"}`,
-            "method": "POST"
-        });
+try {
+    getData().then(data => {
         
-        const data = await res.json();
-        return data
-    }
-
-    const checking = details.filter(a => a.id == userID)[0]
-    console.log(`\nChecking Result of ${checking.name}!\n`)
-    ipo.map(user => {
-        if(user.boid){
-            result(user.boid).then(r => console.log(user.boid, '=>', r.message))
+        const details = []
+        data.body.map(company => {
+            details.push({id:company.id, name:company.name })
+        })
+        
+        details.map(a => console.log(a.id, '=>', a.name))
+        console.log('')
+        const userID = prompt("Choose: ")
+    
+        const result = async (v) => {
+            const res = await fetch(url + "result/result/check", {
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": `{"companyShareId":"${userID}","boid":"${v}"}`,
+                "method": "POST"
+            });
+            
+            const data = await res.json();
+            return data
         }
+    
+        const checking = details.filter(a => a.id == userID)[0]
+        console.log(`\nChecking Result of ${checking.name}!\n`)
+        ipo.map(user => {
+            if(user.boid){
+                result(user.boid).then(r => console.log(user.boid, '=>', r.message))
+            }
+        })
     })
-})
-
+} catch(err) {
+    console.log('Error:', err)
+    process.exit(1)
+}   
